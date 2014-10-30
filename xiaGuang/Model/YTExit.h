ed2b8c8cc2d7e83e3e5a0381ad64b3c6e77d0ad9
Yuan Tao <robotaoyuan@gmail.com>
2014-10-30 19:39:20 +0800

    YTExit

---
+++ xiaGuang/Model/YTExit.h
@@ -0,0 +1,22 @@
+//
+//  Header.h
+//  HighGuang
+//
+//  Created by Yuan Tao on 10/29/14.
+//  Copyright (c) 2014 Yuan Tao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "YTMajorArea.h"
+#import "YTMinorArea.h"
+#import "YTPoiSource.h"
+
+@protocol YTExit <NSObject,YTPoiSource>
+
+@property (nonatomic,weak) NSString *identifier;
+@property (nonatomic,assign) CLLocationCoordinate2D coordinate;
+@property (nonatomic,weak) id<YTMajorArea> majorArea;
+@property (nonatomic,weak) id<YTMinorArea> inMinorArea;
+@property (nonatomic,weak) NSNumber *displayLevel;
+
+@end
