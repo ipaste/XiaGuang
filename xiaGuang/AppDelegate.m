5815d54a7ca0103b85532f8881e699577608ed26
Meng Hu <supermenghu@gmail.com>
2014-11-10 11:13:30 +0800

    1) update major area's world to map ratios 2) adjust timeout values in YTDBManager and AppDelegate

--- xiaGuang/AppDelegate.m
+++ xiaGuang/AppDelegate.m
@@ -53,7 +53,7 @@
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     double now = [[NSDate date] timeIntervalSinceReferenceDate];
     
-    if (now - _timeInToBackground >= 1) { // 20 minutes wait
+    if (now - _timeInToBackground >= 600) { // 10 minutes wait
         self.window = [[UIWindow alloc]init];
         self.window.frame = [UIScreen mainScreen].bounds;
         self.window.backgroundColor = [UIColor blackColor];
