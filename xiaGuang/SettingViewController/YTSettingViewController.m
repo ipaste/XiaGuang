310fb056986153d3da0aa814a87df0a40486f263
theLittleI <451950768@qq.com>
2015-04-16 10:03:34 +0800

    change settingVC

--- xiaGuang/SettingViewController/YTSettingViewController.m
+++ xiaGuang/SettingViewController/YTSettingViewController.m
@@ -25,7 +25,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     _duration = 0.0f;
-    self.isLatest = false;
     _isShowAnimation = YES;
     _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(self.navigationController.navigationBar.frame), CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame) - CGRectGetMaxY(self.navigationController.navigationBar.frame)) style:UITableViewStyleGrouped];
     _tableView.delegate = self;
@@ -36,7 +35,6 @@
     self.navigationItem.title = @"设置";
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:[self leftBarButton]];
     [self.navigationController.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
-    self.navigationController.navigationBar.clipsToBounds = true;
     [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObject:[UIColor colorWithString:@"e65e37"] forKey:NSForegroundColorAttributeName]];
     self.view.layer.contents = (id)[UIImage imageNamed:@"bg_inner.jpg"].CGImage;
     self.automaticallyAdjustsScrollViewInsets = false;
@@ -57,7 +55,7 @@
             return 2;
         case 1:
             
-            return 2;
+            return 5;
     }
     return 0;
     
@@ -81,24 +79,56 @@
     }else if (indexPath.section == 0) {
         switch (indexPath.row) {
             case 0:
-                cell.textLabel.text = @"给虾逛评分";
+                cell.textLabel.text = @"商城地图管理";
                 cell.imageView.image = [UIImage imageNamed:@"icon_2"];
                 break;
             case 1:
-                cell.textLabel.text = @"意见反馈";
+                cell.textLabel.text = @"新手指引";
                 cell.imageView.image = [UIImage imageNamed:@"icon_3"];
                 break;
         }
     }else if (indexPath.section == 1) {
         switch (indexPath.row) {
             case 0:
-                cell.textLabel.text = @"关于虾逛";
-                cell.imageView.image = [UIImage imageNamed:@"icon_5"];
+            {
+                cell.textLabel.text = @"版本更新";
+                cell.imageView.image = [UIImage imageNamed:@"icon_4"];
+                if (!self.isLatest) {
+                    UIButton *latestBtn  = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 45, 20)];
+                    latestBtn.center = CGPointMake(CGRectGetWidth(self.view.frame) - 50, 22);
+                    [latestBtn setTitle:@"新版本" forState:UIControlStateNormal];
+                    [latestBtn.titleLabel setFont:[UIFont systemFontOfSize:11]];
+                    [latestBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+                    [latestBtn setBackgroundColor:[UIColor colorWithString:@"e95e37"]];
+                    latestBtn.layer.cornerRadius = CGRectGetHeight(latestBtn.frame) / 2;
+                    [cell addSubview:latestBtn];
+                }else{
+                    cell.accessoryType = UITableViewCellAccessoryNone;
+                    cell.detailTextLabel.text = @"已是最新版本";
+                    cell.detailTextLabel.textColor = [UIColor colorWithString:@"909090"];
+                    cell.detailTextLabel.font = [UIFont systemFontOfSize:14];
+                    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+                }
+                
+            }
                 break;
             case 1:
-                cell.textLabel.text = @"用户协议";
+                cell.textLabel.text = @"给虾逛评分";
+                cell.imageView.image = [UIImage imageNamed:@"icon_5"];
+                break;
+            case 2:
+                cell.textLabel.text = @"关于虾逛";
                 cell.imageView.image = [UIImage imageNamed:@"icon_6"];
+                //_isShowAnimation = NO;
+                break;
+            case 3:
+                cell.textLabel.text = @"用户协议";
+                cell.imageView.image = [UIImage imageNamed:@"icon_7"];
                 _isShowAnimation = NO;
+                break;
+            case 4:
+                cell.textLabel.text = @"意见反馈";
+                cell.imageView.image =[UIImage imageNamed:@"icon_8"];
                 break;
         }
     }
@@ -143,10 +173,23 @@
         switch (indexPath.row) {
             case 0:
             {
-                controller = [[YTAboutViewController alloc]init];
+                cell.textLabel.text = @"版本更新";
+                if (!self.isLatest) {
+                    //跳转AppStore
+                    NSString *url = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/app/id%d",922405498];
+                    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
+                }else{
+                    return;
+                }
+                
             }
                 break;
             case 1:
+            {
+                controller = [[YTAboutViewController alloc]init];
+            }
+                break;
+            case 2:
             {
                 controller = [[YTUserAgreementViewController alloc]init];
                
@@ -171,11 +214,6 @@
 
 -(void)back:(UIButton *)sender{
     [self.navigationController popViewControllerAnimated:true];
-}
-
--(void)setIsLatest:(BOOL)isLatest{
-    _isLatest = isLatest;
-    [_tableView reloadData];
 }
 
 -(void)dealloc{
